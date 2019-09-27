9407cf8bf397b83f7d10d8203bf52b5ad243e595
Rafael Couto <rafaelcsilva@outlook.com>
2019-09-27 16:49:51 -0300

    front-end_puro

---
+++ front-end_puro/dropdown/dropwdown.js
@@ -0,0 +1,8 @@
+const drop = document.querySelector("#list")
+const arrow = document.querySelector("#arrow")
+
+function dropdown(){
+
+    drop.classList.toggle("ativo")
+    arrow.classList.toggle("rotate")
+}
